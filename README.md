6c1c998dc3e4521b99aa7b4ca0d4d145580711cc
Nick Foden <nickfoden@gmail.com>
2021-12-01 15:04:32 -0500

    day 1 sample

---
+++ README.md
@@ -0,0 +1,5 @@
+https://adventofcode.com/2021
+
+### Solving Advent of Code 2021
+
+When time allows :-)
---
+++ day1_test.go
@@ -0,0 +1,30 @@
+package adventOfCode
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func checkIncrements(data []int64) int64 {
+	result := int64(0)
+	iterator := int64(0)
+
+	for index, v := range data {
+		if index != 0 && v > int64(iterator) {
+			result++
+		}
+		iterator = v
+	}
+
+	return result
+}
+
+func TestDay1(t *testing.T) {
+	// Test the first example
+	sampleInput := []int64{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
+
+	if diff := cmp.Diff(checkIncrements(sampleInput), int64(7)); diff != "" {
+		t.Errorf("Value mismatch (-want +got):\n%s", diff)
+	}
+}
