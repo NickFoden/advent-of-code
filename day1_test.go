f5ad0c0450689bb5402ef4006936e6e2837437af
Nick Foden <nickfoden@gmail.com>
2021-12-02 09:32:43 -0500

    Parse a text file for Day1 (#2)

--- day1_test.go
+++ day1_test.go
@@ -1,6 +1,9 @@
 package adventOfCode
 
 import (
+	"bufio"
+	"os"
+	"strconv"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -20,11 +23,33 @@ func checkIncrements(data []int64) int64 {
 	return result
 }
 
+func readLines(path string) ([]int64, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []int64
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		val, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+		lines = append(lines, val)
+	}
+	return lines, scanner.Err()
+}
+
 func TestDay1(t *testing.T) {
 	// Test the first example
 	sampleInput := []int64{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
 
 	if diff := cmp.Diff(checkIncrements(sampleInput), int64(7)); diff != "" {
+		t.Errorf("Value mismatch (-want +got):\n%s", diff)
+	}
+
+	b, _ := readLines("day1input.txt")
+
+	if diff := cmp.Diff(checkIncrements(b), int64(1722)); diff != "" {
 		t.Errorf("Value mismatch (-want +got):\n%s", diff)
 	}
 }
