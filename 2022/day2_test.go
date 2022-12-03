7d323c90d5ccae7b30fdae796a71c25d8b4a0950
Nick Foden <nickfoden@gmail.com>
2022-12-03 08:10:48 -0500

    Finish day 3

---
+++ 2022/day2_test.go
@@ -0,0 +1,37 @@
+package adventOfCode
+
+import (
+	"testing"
+)
+
+func day2(data []string) int64 {
+	max := int64(0)
+
+	return max
+}
+
+func TestDay2(t *testing.T) {
+
+	// sampleInput, _ := readStringLines("./inputs/2sample.txt")
+	// puzzleInput, _ := readLines("./inputs/2.txt")
+
+	// Part 1
+
+	// if diff := cmp.Diff(day2(sampleInput), int64(24000)); diff != "" {
+	// 	t.Errorf("Value mismatch (-want +got):\n%s", diff)
+	// }
+
+	// if diff := cmp.Diff(mostCalories(puzzleInput), int64(70116)); diff != "" {
+	// 	t.Errorf("Value mismatch (-want +got):\n%s", diff)
+	// }
+
+	// Part 2
+
+	// if diff := cmp.Diff(topThreeMostCalories(sampleInput), int64(45000)); diff != "" {
+	// 	t.Errorf("Value mismatch (-want +got):\n%s", diff)
+	// }
+
+	// if diff := cmp.Diff(topThreeMostCalories(puzzleInput), int64(206582)); diff != "" {
+	// 	t.Errorf("Value mismatch (-want +got):\n%s", diff)
+	// }
+}
