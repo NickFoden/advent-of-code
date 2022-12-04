0b094fc7e597ff3cf2d778a6ec165102241fc6c2
Nick Foden <nickfoden@gmail.com>
2022-12-04 05:35:46 -0500

    Finish day 4

--- 2022/day4_test.go
+++ 2022/day4_test.go
@@ -39,6 +39,34 @@ func campCleanup(data []string) int64 {
 	return result
 }
 
+func campCleanupAnyOverlap(data []string) int64 {
+	result := int64(0)
+
+	for _, day := range data {
+		pairs := strings.Split(day, ",")
+		first := strings.Split(pairs[0], "-")
+		second := strings.Split(pairs[1], "-")
+
+		low1, _ := strconv.Atoi(first[0])
+		high1, _ := strconv.Atoi(first[1])
+
+		low2, _ := strconv.Atoi(second[0])
+		high2, _ := strconv.Atoi(second[1])
+
+		// 53-91,34-53
+		if low1 >= low2 && low1 <= high2 ||
+			low2 >= low1 && low2 <= high1 {
+			result = result + 1
+		} else if high1 <= high2 && high1 >= low2 ||
+			high2 <= high1 && high2 >= low1 {
+			result = result + 1
+		}
+
+	}
+
+	return result
+}
+
 func TestDay4(t *testing.T) {
 
 	sampleInput, _ := readStringLines("./inputs/4sample.txt")
@@ -50,17 +78,17 @@ func TestDay4(t *testing.T) {
 		t.Errorf("Value mismatch (-want +got):\n%s", diff)
 	}
 
-	if diff := cmp.Diff(campCleanup(puzzleInput), int64(8252)); diff != "" {
+	if diff := cmp.Diff(campCleanup(puzzleInput), int64(562)); diff != "" {
 		t.Errorf("Value mismatch (-want +got):\n%s", diff)
 	}
 
 	// Part 2
 
-	// if diff := cmp.Diff(ruckSackBadges(sampleInput), int64(70)); diff != "" {
-	// 	t.Errorf("Value mismatch (-want +got):\n%s", diff)
-	// }
+	if diff := cmp.Diff(campCleanupAnyOverlap(sampleInput), int64(4)); diff != "" {
+		t.Errorf("Value mismatch (-want +got):\n%s", diff)
+	}
 
-	// if diff := cmp.Diff(ruckSackBadges(puzzleInput), int64(2828)); diff != "" {
-	// 	t.Errorf("Value mismatch (-want +got):\n%s", diff)
-	// }
+	if diff := cmp.Diff(campCleanupAnyOverlap(puzzleInput), int64(924)); diff != "" {
+		t.Errorf("Value mismatch (-want +got):\n%s", diff)
+	}
 }
